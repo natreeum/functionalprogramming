37709bf84f0dd1dba0ffe1ab267d6f6d2052ffc0
natreeum <projectkmscom@gmail.com>
2023-08-09 13:49:19 +0900

    reduce : effeciency check

--- reduceNop.js
+++ reduceNop.js
@@ -1,14 +1,30 @@
-const { pReduce1, go, log, nop } = require("./modules");
+const { pReduce1, go, log, takeAll, take } = require("./modules");
 const L = require("./Lazy");
 
 const add = (a, b) => {
   return a + b;
 };
 
+// go(
+//   [1, 2, 3, 4, 5],
+//   L.map((a) => Promise.resolve(a * a)),
+//   L.filter((a) => Promise.resolve(a % 2)),
+//   pReduce1(add),
+//   log
+// );
+
+// latency
 go(
-  [1, 2, 3, 4, 5],
-  L.map((a) => Promise.resolve(a * a)),
-  L.filter((a) => Promise.resolve(a % 2)),
-  pReduce1(add),
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  L.map((a) => {
+    log(a);
+    return new Promise((resolve) => setTimeout(() => resolve(a * a), 1000));
+  }),
+  L.filter((a) => {
+    log(a);
+    return new Promise((resolve) => setTimeout(() => resolve(a % 2), 1000));
+  }),
+  // takeAll,
+  take(2),
   log
 );
